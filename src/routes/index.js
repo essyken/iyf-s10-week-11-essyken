453800731dea36a50134f21c298299754f44bfb7
essyken <njorogeesther356@gmail.com>
2026-04-28 23:41:31 +0300

    Add auth controller and registration/login routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const postsRoutes = require('./posts');
+const authRoutes = require('./auth');
 
 router.use('/posts', postsRoutes);
+router.use('/auth', authRoutes);
 
 router.get('/health', (req, res) => {
     res.json({ status: 'OK' });
