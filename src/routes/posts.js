b1821dc10f3f275ce1ec4078c8392b33709cbe89
essyken <njorogeesther356@gmail.com>
2026-04-28 23:52:08 +0300

    Add auth middleware and protect post routes

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,17 +2,24 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/postsController');
 const commentsController = require('../controllers/commentsController');
+const { protect, restrictTo } = require('../middleware/auth');
 
+// Public routes
 router.get('/', postsController.getAllPosts);
 router.get('/:id', postsController.getPostById);
-router.post('/', postsController.createPost);
-router.put('/:id', postsController.updatePost);
-router.delete('/:id', postsController.deletePost);
+
+// Protected routes
+router.post('/', protect, postsController.createPost);
+router.put('/:id', protect, postsController.updatePost);
+router.delete('/:id', protect, postsController.deletePost);
 router.patch('/:id/like', postsController.likePost);
+
+// Admin only
+router.delete('/:id/force', protect, restrictTo('admin'), postsController.deletePost);
 
 // Comment routes nested under posts
 router.get('/:postId/comments', commentsController.getComments);
-router.post('/:postId/comments', commentsController.createComment);
-router.delete('/:postId/comments/:commentId', commentsController.deleteComment);
+router.post('/:postId/comments', protect, commentsController.createComment);
+router.delete('/:postId/comments/:commentId', protect, commentsController.deleteComment);
 
 module.exports = router;
